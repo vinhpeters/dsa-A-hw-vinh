91d6ae48b7946148cc0da68d783092efe9096ffd
vinhp <vinhpeters@outlook.com>
2022-12-02 12:27:16 -0500

    Complete bonus problems in python and js

--- assignment_3.js
+++ assignment_3.js
@@ -94,3 +94,126 @@ console.log(binaryIterativeSearch(nums, target))
 nums = [-1, 0, 3, 5, 9, 12]
 target = 2
 console.log(binaryIterativeSearch(nums, target))
+
+// Extra Problems
+
+// Problem 1
+
+/*
+Psuedocode
+Sort array. For each pair, count numbers in array while sum pair > number.
+
+
+def triangleNumber(nums):
+    triplets = 0
+    nums.sort()
+    # Loop for first number
+    for i in range(len(nums)-2):
+        # Loop for second number
+        for j in range(i+1,len(nums)-1):
+            k= j + 1
+            while nums[k] < (nums[i]+nums[j]) and k < len(nums)
+                k++
+            triplets += k-j-1
+            
+    return triplets
+
+*/
+
+const triangleNumber = (nums) => {
+    let triplets = 0;
+    nums.sort();
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            let end = nums.length - 1;
+            let start = j + 1;
+
+            while (start <= end) {
+                let mid = ~~((start + end) / 2);
+                if (nums[mid] >= nums[i] + nums[j]) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+            triplets += end - j;
+        }
+    }
+    return triplets;
+}
+console.log('triangleNumber')
+
+nums = [2, 2, 3, 4]
+console.log(triangleNumber(nums))
+
+nums = [4, 2, 3, 4]
+console.log(triangleNumber(nums))
+
+// Alt version
+
+/* Pseudocode
+Sort array. For each pair, count numbers in array while sum pair > number. 
+*/
+
+const triangleNumberCount = (nums) => {
+    let triplets = 0;
+    nums.sort();
+    for (let i = 0; i < nums.length - 2; i++) {
+
+        for (let j = i + 1; j < nums.length - 1; j++) {
+
+            let k = j + 1;
+            while (k < nums.length && nums[k] < nums[i] + nums[j]) {
+
+                k += 1;
+            }
+            triplets += k - j - 1;
+
+        }
+    }
+    return triplets;
+}
+
+console.log('Triangle number v2')
+
+nums = [2, 2, 3, 4]
+console.log(triangleNumberCount(nums))
+
+nums = [4, 2, 3, 4]
+console.log(triangleNumberCount(nums))
+
+
+// Problem 2
+/* Pseudocode
+
+Use a dict to count the frequency of each element. If count value is greater than 1, add number to output list. 
+
+def findDuplicates(nums)
+    freqs={}
+    output = []
+    # Loop through list
+    for num in nums
+        freqs[num] = freqs[num] + 1 if num in freqs else 1
+        ## Save number to output as soon as freq > 1
+        if freqs[num] > 1
+            output.append(num)
+    return output
+
+ */
+
+const findDuplicates = (nums) => {
+    const output = [];
+    freqs = new Map();
+    for (let num of nums) {
+        (freqs.get(num) ? freqs.set(num, freqs.get(num) + 1) : freqs.set(num, 1));
+        if (freqs.get(num) > 1) {
+            output.push(num)
+        }
+    }
+    return output;
+}
+
+console.log('Find duplicates')
+nums = [4,3,2,7,8,2,3,1]
+console.log(findDuplicates(nums))
